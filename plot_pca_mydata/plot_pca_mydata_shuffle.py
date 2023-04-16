a6075cba5a387958296048d296923359b1a5e66e
Jasper Wong <yom092090@gmail.com>
2023-04-16 22:49:50 +0800

    Parse samples from folder

--- plot_pca_mydata/plot_pca_mydata_shuffle.py
+++ plot_pca_mydata/plot_pca_mydata_shuffle.py
@@ -2,17 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import decomposition
 from sklearn import model_selection
+import glob
+import csv
+import os
 
-arr1 = np.genfromtxt('PET_Dettol_2_touching.csv', delimiter=',')
+PET_list = list()
+for filename in os.listdir("PET"):
+    filename = "PET\\" + filename
+    with open(filename, 'r') as file:
+        PET_list += list(csv.reader(file, delimiter=','))
+PP_list = list()
+for filename in os.listdir("PP"):
+    filename = "PP\\" + filename
+    with open(filename, 'r') as file:
+        PP_list += list(csv.reader(file, delimiter=','))
+
+arr1 = np.asarray(PET_list)
 arr1 = np.delete(arr1, 10, 1)
 arr1_offseted = arr1.copy()
-for ar in arr1_offseted:
-    ar -= ar[-1]
-arr2 = np.genfromtxt('PP_tofulism_2_touching.csv', delimiter=',')
+# for ar in arr1_offseted:
+#     ar -= ar[-1]
+arr2 = np.asarray(PP_list)
 arr2 = np.delete(arr2, 10, 1)
 arr2_offseted = arr2.copy()
-for ar in arr2_offseted:
-    ar -= ar[-1]
+# for ar in arr2_offseted:
+#     ar -= ar[-1]
 
 X = np.vstack((arr1_offseted, arr2_offseted))
 sample_pair = [("PET", 1), ("PP", 5)]
