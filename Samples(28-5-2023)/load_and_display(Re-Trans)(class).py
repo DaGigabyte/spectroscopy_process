50cb5c4f948292a64f7d3a307286c76d24b742b1
Jasper Wong <yom092090@gmail.com>
2023-05-29 01:05:45 +0800

    28-5-2023 class only

---
+++ Samples(28-5-2023)/load_and_display(Re-Trans)(class).py
@@ -0,0 +1,47 @@
+import numpy as np
+from sklearn import decomposition, preprocessing
+import joblib
+import serial
+ser = serial.Serial(port="COM7", baudrate=115200, timeout=5)
+print(ser.name)
+
+trans_index = np.array([1, 4, 5, 7])
+reflectance_index = np.array([0, 2, 3, 6, 8])
+# (empty_or_not_pipe, classify_pipe) = joblib.load('trained_pipes(Re-Trans)(Slanted)(proba).joblib')
+((Both_empty_or_not_pipe, Both_classify_pipe), (Re_empty_or_not_pipe, Re_classify_pipe), (Trans_empty_or_not_pipe, Trans_classify_pipe)) = joblib.load('trained_pipes(28-5-2023)v4.joblib')
+
+pipe_containers = dict()
+pipe_containers['Both_empty'] = Both_empty_or_not_pipe
+pipe_containers['Both_classify'] = Both_classify_pipe
+pipe_containers['Re_empty'] = Re_empty_or_not_pipe
+pipe_containers['Re_classify'] = Re_classify_pipe
+pipe_containers['Trans_empty'] = Trans_empty_or_not_pipe
+pipe_containers['Trans_classify'] = Trans_classify_pipe
+
+str_data = ''
+while not ser.in_waiting:
+    pass
+while True:
+    while ser.in_waiting:
+        str_data = ser.readline().strip().decode()
+    input_arr = str_data.split("\t")
+    num_arr = [n.split(':')[1] for n in input_arr]
+    arr_both = np.array(list(map(float, num_arr[0:9])))
+    arr_Re = arr_both[reflectance_index]
+    arr_Trans = arr_both[trans_index]
+    arrs = [arr_both, arr_both, arr_both[reflectance_index], arr_both[reflectance_index],  arr_both[trans_index], arr_both[trans_index]]
+    # print(Re_empty_or_not_pipe.classes_, Re_classify_pipe.classes_, Trans_empty_or_not_pipe.classes_, Trans_classify_pipe.classes_, end='\t')
+    # print("Both:", ["{:.3f}".format(p) for p in Both_empty_or_not_pipe.predict_proba(arr_both.reshape(1, -1))[0]], ["{:.3f}".format(p) for p in Both_classify_pipe.predict_proba(arr_both.reshape(1, -1))[0]], end=' ')
+    # # print("Re:", ["{:.3f}".format(p) for p in Re_empty_or_not_pipe.predict_proba(arr_Re.reshape(1, -1))[0]], ["{:.3f}".format(p) for p in Re_classify_pipe.predict_proba(arr_Re.reshape(1, -1))[0]], end=' ')
+    # # print("Trans:", ["{:.3f}".format(p) for p in Trans_empty_or_not_pipe.predict_proba(arr_Trans.reshape(1, -1))[0]], ["{:.3f}".format(p) for p in Trans_classify_pipe.predict_proba(arr_Trans.reshape(1, -1))[0]])
+    # for container, pipe, arr in zip(bar_containers.values(), pipe_containers.values(), arrs):
+    #     print(pipe)
+    #     for rect, h in zip(container, pipe.predict_proba(arr.reshape(1, -1))[0]):
+    #         rect.set_height(h)
+    
+    
+    # print(empty_or_not_pipe.predict_proba(arr.reshape(1, -1)), classify_pipe.predict_proba(arr.reshape(1, -1)))
+    # print(empty_or_not_pipe.predict(arr.reshape(1, -1)), classify_pipe.predict(arr.reshape(1, -1)))
+
+    print(Both_empty_or_not_pipe.predict(arr_both.reshape(1, -1)), Both_classify_pipe.predict(arr_both.reshape(1, -1)), end='\t')
+    print(input_arr)
---
+++ Samples(28-5-2023)/train_and_store(Re-Trans).py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import decomposition, preprocessing, pipeline, svm, model_selection
+import joblib
+import csv
+import os
+
+Empty_Re_dir = "Empty_Re"
+Empty_Trans_dir = "Empty_Trans"
+PET_Trans_dir = "PET_Trans"
+PP_Trans_dir = "PP_Trans"
+PET_Re_dir = "PET_Re"
+PP_Re_dir = "PP_Re"
+Empty_Both_dir = "Empty_Both"
+PET_Both_dir = "PET_Both"
+PP_Both_dir = "PP_Both"
+
+def dir_as_list(dir):
+    output = list()
+    for filename in os.listdir(dir):
+        filename = dir + "\\" + filename
+        with open(filename, 'r') as file:
+            my_list = [list(map(float, row[:-1])) for row in csv.reader(file, delimiter=',')]
+            output += my_list
+    return output
+
+def dictOfList_as_dataset(my_dict):
+    X, Y = [], []
+    for key, value in my_dict.items():
+        X.extend(value)
+        Y.extend([key] * len(value))
+    return np.asarray(X), np.asarray(Y)
+
+def EmptyOrNotPipeline(X, Y):
+    pipe = pipeline.Pipeline([('svc', svm.SVC(gamma='auto', kernel='linear', probability=True)), ], verbose=True)
+    pipe.fit(X, Y)
+    return pipe
+
+def classifyPipeline(X, Y):
+    pipe = pipeline.Pipeline([('sc', preprocessing.StandardScaler()), ('pca', decomposition.PCA(n_components=4)), ('svc', svm.SVC(gamma='auto', kernel='rbf', probability=True))], verbose=True)
+    pipe.fit(X, Y)
+    return pipe
+
+def dirsToPipe(empty_dir, PET_dir, PP_dir):
+    Re_withEmpty = {'Empty': dir_as_list(empty_dir), 'PET': dir_as_list(PET_dir), 'PP': dir_as_list(PP_dir)}
+    Re = {'PET': dir_as_list(PET_dir), 'PP': dir_as_list(PP_dir)}
+    X, Y = dictOfList_as_dataset(Re_withEmpty)
+    Y_emptyornot = np.array(['Empty' if label == 'Empty' else 'Non-Empty' for label in Y])
+    print("EmptyOrNotPipeline")
+    empty_or_not_pipe = EmptyOrNotPipeline(X, Y_emptyornot)
+    X, Y = dictOfList_as_dataset(Re)
+    print("classifyPipeline", np.shape(Y))
+    classify_pipe = classifyPipeline(X, Y)
+    return empty_or_not_pipe, classify_pipe
+
+Both_empty_or_not_pipe, Both_classify_pipe = dirsToPipe(Empty_Both_dir, PET_Both_dir, PP_Both_dir)
+Re_empty_or_not_pipe, Re_classify_pipe = dirsToPipe(Empty_Re_dir, PET_Re_dir, PP_Re_dir)
+Trans_empty_or_not_pipe, Trans_classify_pipe = dirsToPipe(Empty_Trans_dir, PET_Trans_dir, PP_Trans_dir)
+joblib.dump(((Both_empty_or_not_pipe, Both_classify_pipe), (Re_empty_or_not_pipe, Re_classify_pipe), (Trans_empty_or_not_pipe, Trans_classify_pipe)), 'trained_pipes(28-5-2023)v4.joblib')
